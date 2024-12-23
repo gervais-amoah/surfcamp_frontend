954bed68cfef2a4eab1bd3a9f3f6ded8c5b1e8a6
gervais <gervaisamoah@yahoo.com>
2024-12-23 20:23:05 +0000

    Info-block component's template

---
+++ app/_components/InfoBlock.jsx
@@ -0,0 +1,21 @@
+import Image from 'next/image';
+import Link from 'next/link';
+import React from 'react';
+
+export default function InfoBlock({ data }) {
+  const { headline, text, button, reversed } = data;
+
+  return (
+    <div className={`info ${reversed ? 'info--reversed' : ''}`}>
+      <div className="info__image">
+        <Image src="/info-block/rectangle.png" fill={true} alt="Info Block" />
+      </div>
+
+      <div className="info__text">
+        <h2 className="info__headline">{headline}</h2>
+        {text}
+        <Link href="/">{button}</Link>
+      </div>
+    </div>
+  );
+}
--- app/page.jsx
+++ app/page.jsx
@@ -1,4 +1,5 @@
 import HeroSection from './_components/HeroSection';
+import InfoBlock from './_components/InfoBlock';
 
 export default function Home() {
   const heroHeadline = (
@@ -9,9 +10,36 @@ export default function Home() {
     </>
   );
 
+  const infoBlockData = {
+    headline: 'the experience.',
+    text: (
+      <p className="copy">
+        At Sam’s Surfcamp, we invite you to embark on an unforgettable surfing
+        adventure. Nestled in the heart of [Location] our surf camp offers an
+        exhilarating experience for beginners, intermediate surfers, and
+        seasoned wave riders alike. Dive into the world of surfing with our
+        expert instructors who have years of experience and a deep passion for
+        the sport. Whether you&apos;re a first-time surfer looking to catch your
+        first wave or a seasoned pro seeking to enhance your skills, our
+        dedicated team is here to guide you every step of the way. Immerse
+        yourself in the natural beauty of our surf camp&apos;s surroundings.
+        Picture yourself waking up to the sound of crashing waves and feeling
+        the warm sand beneath your feet. With pristine beaches and a vibrant
+        coastal atmosphere, [Location] sets the perfect stage for your surf
+        adventure.
+      </p>
+    ),
+    button: (
+      <button className="btn btn--turquoise btn--medium">BOOK NOW</button>
+    ),
+    reversed: false,
+  };
+
   return (
     <>
       <HeroSection headline={heroHeadline} />
+      <InfoBlock data={infoBlockData} />
+      <InfoBlock data={{ ...infoBlockData, reversed: true }} />
     </>
   );
 }
