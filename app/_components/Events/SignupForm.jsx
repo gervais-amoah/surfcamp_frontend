bac6b9d6cc9c07753d05d3023cdf2d2ba3def3c1
gervais <gervaisamoah@yahoo.com>
2024-12-28 11:16:29 +0000

    Show confirmation message

--- app/_components/Events/SignupForm.jsx
+++ app/_components/Events/SignupForm.jsx
@@ -12,32 +12,48 @@ export default function SignupForm({ headline, infoText, btnLabel }) {
     phone: '',
   });
 
+  const [showConfirmation, setShowConfirmation] = useState(false);
+
   const onChange = (e) => {
     setFormData({ ...formData, [e.target.name]: e.target.value });
   };
 
   const onSubmit = async (e) => {
     e.preventDefault();
-    console.log(formData);
-    // send data to strapi
-    // 1. create new participant with general interest
-    const participantObj = {
-      data: {
-        email: formData.email,
-        firstName: formData.firstName,
-        lastName: formData.lastName,
-        phone: formData.phone,
-        isGeneralInterest: true,
-      },
-    };
-    const participant = await fetch(API_URL + '/participants', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(participantObj),
-    });
-    // 2. create new newsletter
+
+    try {
+      const participant = await fetch(API_URL + '/participants', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          data: {
+            ...formData,
+            isGeneralInterest: true,
+          },
+        }),
+      });
+      if (participant.ok) {
+        setShowConfirmation(true);
+        setFormData({
+          firstName: '',
+          lastName: '',
+          email: '',
+          phone: '',
+        });
+
+        setTimeout(() => {
+          setShowConfirmation(false);
+        }, 4000);
+      } else {
+        alert('Something went wrong. Please try again later.');
+      }
+    } catch (err) {
+      console.error(err);
+      const errorMessage = err.response?.data?.error?.message;
+      alert(errorMessage || 'Something went wrong. Please try again later.');
+    }
   };
 
   return (
@@ -47,50 +63,60 @@ export default function SignupForm({ headline, infoText, btnLabel }) {
         {infoText}
       </div>
 
-      <form action="#" className="signup-form__form" onSubmit={onSubmit}>
-        <div className="signup-form__name-container">
+      {showConfirmation ? (
+        <div className="signup-form__form">
+          <h4>Thank you for signing up!</h4>
+          <p className="copy">
+            You will receive an email from us shortly with more information
+            about our upcoming events.
+          </p>
+        </div>
+      ) : (
+        <form action="#" className="signup-form__form" onSubmit={onSubmit}>
+          <div className="signup-form__name-container">
+            <TextInput
+              isRequired={true}
+              inputName="firstName"
+              inputType="text"
+              label="First Name"
+              value={formData.firstName}
+              onChange={onChange}
+            />
+            <TextInput
+              isRequired={true}
+              inputName="lastName"
+              inputType="text"
+              label="Last Name"
+              value={formData.lastName}
+              onChange={onChange}
+            />
+          </div>
           <TextInput
             isRequired={true}
-            inputName="firstName"
-            inputType="text"
-            label="First Name"
-            value={formData.firstName}
+            inputName="email"
+            inputType="email"
+            label="Your e-mail address"
+            value={formData.email}
             onChange={onChange}
           />
           <TextInput
             isRequired={true}
-            inputName="lastName"
-            inputType="text"
-            label="Last Name"
-            value={formData.lastName}
+            inputName="phone"
+            inputType="phone"
+            label="Your telephone number"
+            value={formData.tel}
             onChange={onChange}
           />
-        </div>
-        <TextInput
-          isRequired={true}
-          inputName="email"
-          inputType="email"
-          label="Your e-mail address"
-          value={formData.email}
-          onChange={onChange}
-        />
-        <TextInput
-          isRequired={true}
-          inputName="phone"
-          inputType="phone"
-          label="Your telephone number"
-          value={formData.tel}
-          onChange={onChange}
-        />
-        <div className="signup-form__submit-container">
-          <button
-            type="submit"
-            className="btn btn--medium btn--turquoise signup-form__submit-btn"
-          >
-            {btnLabel || 'Stay in touch!'}
-          </button>
-        </div>
-      </form>
+          <div className="signup-form__submit-container">
+            <button
+              type="submit"
+              className="btn btn--medium btn--turquoise signup-form__submit-btn"
+            >
+              {btnLabel || 'Stay in touch!'}
+            </button>
+          </div>
+        </form>
+      )}
     </section>
   );
 }
