d4ba4aa4ce4a469c4a815d20a260906b61201487
gervais <gervaisamoah@yahoo.com>
2024-12-28 14:23:13 +0000

    Change the sign-up form style

--- app/_components/Events/SignupForm.jsx
+++ app/_components/Events/SignupForm.jsx
@@ -64,101 +64,101 @@ export default function SignupForm({
   };
 
   return (
-    <section className="signup-form">
-      <div className="signup-form__info">
-        <h3 className="signup-form__headline">{headline}</h3>
-        {infoText}
+    <section>
+      <div className="signup-form">
+        <div className="signup-form__info">
+          <h3 className="signup-form__headline">{headline}</h3>
+          {infoText}
+        </div>
+
+        <div className="signup-form__form">
+          {featuredImage && (
+            <div className="signup-form__image">
+              <Image src={featuredImage} alt={headline} fill />
+            </div>
+          )}
+
+          {showConfirmation ? (
+            <div className="">
+              <h4>Thank you for signing up!</h4>
+              <p className="copy">
+                You will receive an email from us shortly with more information
+                about our upcoming events.
+              </p>
+            </div>
+          ) : (
+            <form action="#" className="" onSubmit={onSubmit}>
+              <div className="signup-form__name-container">
+                <TextInput
+                  isRequired={true}
+                  inputName="firstName"
+                  inputType="text"
+                  label="First Name"
+                  value={formData.firstName}
+                  onChange={onChange}
+                />
+                <TextInput
+                  isRequired={true}
+                  inputName="lastName"
+                  inputType="text"
+                  label="Last Name"
+                  value={formData.lastName}
+                  onChange={onChange}
+                />
+              </div>
+              <TextInput
+                isRequired={true}
+                inputName="email"
+                inputType="email"
+                label="Your e-mail address"
+                value={formData.email}
+                onChange={onChange}
+              />
+              <TextInput
+                isRequired={true}
+                inputName="phone"
+                inputType="phone"
+                label="Your telephone number"
+                value={formData.tel}
+                onChange={onChange}
+              />
+              <div className="signup-form__submit-container">
+                <button
+                  type="submit"
+                  className="btn btn--medium btn--turquoise signup-form__submit-btn"
+                >
+                  {btnLabel || 'Stay in touch!'}
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
       </div>
 
-      <div className="signup-form__form">
-        {featuredImage && (
-          <div className="signup-form__image">
-            <Image src={featuredImage} alt={headline} fill />
-          </div>
-        )}
-
-        {showConfirmation ? (
-          <div className="">
-            <h4>Thank you for signing up!</h4>
+      <div className="signup-form__footer">
+        {pricing && (
+          <div className="signup-form__pricing">
+            <h3>Pricing</h3>
             <p className="copy">
-              You will receive an email from us shortly with more information
-              about our upcoming events.
+              Single Room:{' '}
+              <span className="bold">{pricing.singlePrice}€ per person</span>
+            </p>
+            <p className="copy">
+              Shared Room:{' '}
+              <span className="bold">{pricing.sharedPrice}€ per person</span>
             </p>
           </div>
-        ) : (
-          <form action="#" className="" onSubmit={onSubmit}>
-            <div className="signup-form__name-container">
-              <TextInput
-                isRequired={true}
-                inputName="firstName"
-                inputType="text"
-                label="First Name"
-                value={formData.firstName}
-                onChange={onChange}
-              />
-              <TextInput
-                isRequired={true}
-                inputName="lastName"
-                inputType="text"
-                label="Last Name"
-                value={formData.lastName}
-                onChange={onChange}
-              />
-            </div>
-            <TextInput
-              isRequired={true}
-              inputName="email"
-              inputType="email"
-              label="Your e-mail address"
-              value={formData.email}
-              onChange={onChange}
-            />
-            <TextInput
-              isRequired={true}
-              inputName="phone"
-              inputType="phone"
-              label="Your telephone number"
-              value={formData.tel}
-              onChange={onChange}
-            />
-            <div className="signup-form__submit-container">
-              <button
-                type="submit"
-                className="btn btn--medium btn--turquoise signup-form__submit-btn"
-              >
-                {btnLabel || 'Stay in touch!'}
-              </button>
-            </div>
-
-            {pricing && (
-              <div className="signup-form__pricing">
-                <h3>Pricing</h3>
-                <p className="copy">
-                  Single Room:{' '}
-                  <span className="bold">
-                    {pricing.singlePrice}€ per person
-                  </span>
-                </p>
-                <p className="copy">
-                  Shared Room:{' '}
-                  <span className="bold">
-                    {pricing.sharedPrice}€ per person
-                  </span>
-                </p>
-              </div>
-            )}
-            {date && (
-              <div className="signup-form__date">
-                <h3>Date</h3>
-                <p className="copy">
-                  Startind date: <span className="bold">{date.start}</span>
-                </p>
-                <p className="copy">
-                  End date: <span className="bold">{date.end}</span>
-                </p>
-              </div>
-            )}
-          </form>
+        )}
+        {date && (
+          <div className="signup-form__date">
+            <h3>Date</h3>
+            <p className="copy">
+              Startind date: <span className="bold">{date.start}</span>
+            </p>
+            <p className="copy">
+              End date: <span className="bold">{date.end}</span>
+            </p>
+          </div>
         )}
       </div>
     </section>
